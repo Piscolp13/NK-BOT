ee928bc061b922d9302d1d1a9c7bde3500bbf865
Piscolp13 <90126456+Piscolp13@users.noreply.github.com>
2021-09-05 22:48:46 -0500

    Update muslosss.js

--- fakeapixd/muslosss.js
+++ fakeapixd/muslosss.js
@@ -4,5 +4,10 @@
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7GOqAF5ilzd7mKQQ4ILGzBFDTaiMWuxZt7g&usqp=CAU"},
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2_oZcwwZbu6FLd7es_3FeRVsk0VakmhUjZw&usqp=CAU"},
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5d2lXGZMmnqrG6fDSs9fzfONYjUc4P4PbSg&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOsqbZPXQgW9dP3X6ywfY7Qrf4t2X4SgFzxA&usqp=CAU"}
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOsqbZPXQgW9dP3X6ywfY7Qrf4t2X4SgFzxA&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmwrVEadhCxVETzozHjAfidRoMfZYaERzh-Q&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjrI5Hr0qbDtBiNLKMmoTvunZaTVWRxXRp2w&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTM-cFGqPxWUbZQpuIxq6Pt2Op7qHihIqEXw&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9lgGvi9nlB8ABHHtY6ErqUTqRv1aXN1rOKQ&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgjebSxonOpQDyTM1W_M8x5kFI5vGx8ZG1wg&usqp=CAU"}
 ]
